2c299cbea9b7b720bd73b125bb1feb95b85ec807
Rodrigo Saforcada <ronninn18@gmail.com>
2021-08-19 22:55:22 -0300

    programa que imprime en consola mis datos personales

---
+++ CursoDeJava/src/RodrigoSaforcada.java
@@ -0,0 +1,13 @@
+public class RodrigoSaforcada {
+    public static void main(String[] args) {
+        String nombre = "Rodrigo";
+        int edad = 23;
+        double salario = 40000.05;
+        boolean carnet = true;
+
+        System.out.println("nombre = " + nombre);
+        System.out.println("edad = " + edad);
+        System.out.println("salario = " + salario);
+        System.out.println("carnet = " + carnet);
+    }
+}

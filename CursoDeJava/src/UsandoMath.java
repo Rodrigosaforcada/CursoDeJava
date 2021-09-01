4dbd853cd91a91cb714b9f2cd02a417244916093
Rodrigo Saforcada <ronninn18@gmail.com>
2021-09-01 19:54:28 -0300

    desafio 7 resuelto - API de Java con Math

---
+++ CursoDeJava/src/UsandoMath.java
@@ -0,0 +1,15 @@
+public class UsandoMath {
+    public static void main(String[] args) {
+        double numero = 5;
+        double numero2 = 6;
+        System.out.println("seno = " + Math.sin(numero));
+        System.out.println("coseno = " + Math.cos(numero));
+        System.out.println("tangente = " + Math.tan(numero));
+        System.out.println("arcotangente = " + Math.atan(numero));
+        System.out.println("ángulo theta = " + Math.atan2(numero, numero2));
+        System.out.println("exponente = " + Math.exp(numero));
+        System.out.println("logaritmo = " + Math.log(numero));
+        System.out.println("PI = " + Math.PI);
+        System.out.println("E = " + Math.E);
+    }
+}
